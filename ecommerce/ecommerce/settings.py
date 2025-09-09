15e92780067fc2af19ab753550ddda58440da1b2
Faiaz00 <faiazfahmid6@gmail.com>
2025-09-09 22:30:02 +0600

    UPDATED DATABASE

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -76,12 +76,38 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#mysql
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'male_fashion',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  
+#         'PORT': '3306',       
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'male_fashion',                      
+        'USER': 'postgres',
+        'PASSWORD': 'FFahmid13579',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
